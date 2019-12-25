5c63cff0c9273cc9b594a8f4b99934f4c8808191
chen2016 <201518132@qq.com>
2019-12-25 23:18:51 +0800

    循环队列的go版本实现

---
+++ datastruxt-algoritem-go/datastruct/queue/ring.go
@@ -0,0 +1,84 @@
+package queue
+
+import "math"
+
+// Create by czx on 2019/12/25
+const DefaultCapacity = 16
+
+// RingQueue 循环队列, 我们多创建一个容量便于判断是否队满：make(type, len, cap+1)
+// 添加一个元素时，rear++， 删除一个元素时，front--
+// 队列为空时: front == rear
+// 队列为满： (rear + 1) % cap(element) == front
+// 队列大小：Math.abs(rear-front) % cap(element)
+// 下一个索引位置：(front/rear+1) % cap(element)
+type RingQueue struct {
+	element []interface{}
+	front   int
+	rear    int
+	size    int
+}
+
+func NewRingQueue(n int) *RingQueue {
+	if n <= 0 || n > math.MaxInt64 {
+		panic("n overflow")
+	}
+	return &RingQueue{element: make([]interface{}, n, n+1)}
+}
+
+func (r *RingQueue) Push(e interface{}) {
+	if r.full() {
+		r.resize(cap(r.element)*2 - 1)
+	}
+	r.element[r.rear] = e
+	r.rear++
+	r.size++
+}
+
+func (r *RingQueue) Pop() (bool, interface{}) {
+	if r.Empty() {
+		return false, nil
+	}
+	r.front++
+	r.size--
+	return true, r.element[r.front-1]
+}
+
+func (r *RingQueue) Empty() bool {
+	return r.front == r.rear
+}
+
+func (r *RingQueue) full() bool {
+	return (r.rear+1)%cap(r.element) == r.front
+}
+
+func (r *RingQueue) Size() int {
+	// size = int(math.Abs(float64(r.rear)-float64(r.front))) % cap(r.element)
+	return r.size
+}
+
+func (r *RingQueue) Clear() {
+	r.element = make([]interface{}, DefaultCapacity, DefaultCapacity+1)
+	r.size = 0
+	r.front = 0
+	r.rear = 0
+}
+
+func (r *RingQueue) resize(capacity int) {
+	newElement := make([]interface{}, capacity)
+	curr := r.front
+	for i := 0; curr != r.rear; i++ {
+		newElement[i] = r.element[curr]
+		curr = (curr + 1) % cap(r.element)
+	}
+	r.front = 0
+	r.rear = r.Size()
+	r.element = newElement
+}
+
+func (r *RingQueue) Foreach(fn func(v interface{})) {
+	curr := r.front
+	for curr != r.rear {
+		fn(r.element[curr])
+		curr = (curr + 1) % cap(r.element)
+	}
+}
---
+++ datastruxt-algoritem-go/datastruct/queue/ring_test.go
@@ -0,0 +1,49 @@
+package queue
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Create by czx on 2019/12/25
+
+func TestRingQueue(t *testing.T) {
+	r := NewRingQueue(10)
+	for i := 0; i < 10; i++ {
+		r.Push(i)
+	}
+	r.Foreach(func(v interface{}) {
+		fmt.Printf("%v ", v)
+	})
+	fmt.Println()
+	fmt.Println("size: ", r.Size())
+	r.Push(100)
+	r.Foreach(func(v interface{}) {
+		fmt.Printf("%v ", v)
+	})
+	fmt.Println()
+	fmt.Println("size: ", r.Size())
+	fmt.Println("is empty: ", r.Empty())
+
+	if ok, i := r.Pop(); ok {
+		fmt.Println("pop success: ", i)
+	}
+	if ok, i := r.Pop(); ok {
+		fmt.Println("pop success: ", i)
+	}
+	if ok, i := r.Pop(); ok {
+		fmt.Println("pop success: ", i)
+	}
+
+	r.Clear()
+	fmt.Println("is empty: ", r.Empty())
+	fmt.Println("size: ", r.Size())
+	fmt.Println("capacity: ", cap(r.element))
+	for i := 0; i < 20; i++ {
+		r.Push(i)
+	}
+	r.Foreach(func(v interface{}) {
+		fmt.Printf("%v ", v)
+	})
+	fmt.Println()
+}
