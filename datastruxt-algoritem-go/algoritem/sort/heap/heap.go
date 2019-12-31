0b0b4b154d3f892fb085dc4efa734a514ca36df3
chen2016 <201518132@qq.com>
2020-01-01 00:39:45 +0800

    堆排序-go版本实现

---
+++ datastruxt-algoritem-go/algoritem/sort/heap/heap.go
@@ -0,0 +1,55 @@
+package heap
+
+import "com.datastruct.algoritem.go/algoritem/util"
+
+// Create by czx on 2020/1/1
+
+func SOrt(arr []int) {
+	heapSort(arr)
+}
+
+func heapSort(arr []int) {
+	if len(arr) < 2 {
+		return
+	}
+	// 先转为大顶堆
+	for i := 0; i < len(arr); i++ {
+		Insert(arr, i)
+	}
+
+	// 将0号最大元素和尾部交换，之后从0号开始调整堆
+	size := len(arr)
+	util.Swap(arr, 0, len(arr)-1)
+	size--
+	for size > 0 {
+		heapIfy(arr, 0, size)
+		size--
+		util.Swap(arr, 0, size)
+	}
+}
+
+func Insert(arr []int, i int) {
+	for arr[i] > arr[(i-1)/2] {
+		util.Swap(arr, i, (i-1)/2)
+		i = (i - 1) / 2
+	}
+}
+
+func heapIfy(arr []int, index, size int) {
+	left := 2*index + 1
+	for left < size {
+		max := left
+		if left+1 < size && arr[left+1] > arr[left] {
+			max = left + 1
+		}
+		if arr[max] < arr[index] {
+			max = index
+		}
+		if max == index {
+			break
+		}
+		util.Swap(arr, max, index)
+		index = max
+		left = 2*index + 1
+	}
+}
---
+++ datastruxt-algoritem-go/algoritem/sort/heap/heap_test.go
@@ -0,0 +1,44 @@
+package heap
+
+import (
+	"com.datastruct.algoritem.go/algoritem/util"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+// Create by czx on 2020/1/1
+
+func TestSOrt(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "heap sort",
+			args: args{util.GeneratorArray(10, 1, 10)},
+		},
+		{
+			name: "heap sort",
+			args: args{util.GeneratorArray(1000, 10, 100)},
+		},
+		{
+			name: "heap sort",
+			args: args{util.GeneratorArray(100000, 10, 1000)},
+		},
+		{
+			name: "heap sort",
+			args: args{util.GeneratorArray(10000000, 100, 10000)},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			array := util.SortedArray(tt.args.arr)
+			SOrt(tt.args.arr)
+			fmt.Println(tt.name+" result: ", reflect.DeepEqual(array, tt.args.arr))
+		})
+	}
+}
