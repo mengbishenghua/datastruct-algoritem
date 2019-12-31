1d09e5a61493e6a420e884b9cf9a9b062b392122
chen2016 <201518132@qq.com>
2019-12-31 23:12:51 +0800

    快速排序-go版本实现

--- datastruxt-algoritem-go/algoritem/sort/quick/quick.go
+++ datastruxt-algoritem-go/algoritem/sort/quick/quick.go
@@ -1,6 +1,8 @@
 package quick
 
-import "com.datastruct.algoritem.go/algoritem/util"
+import (
+	"com.datastruct.algoritem.go/algoritem/util"
+)
 
 // Create by czx on 2019/12/31
 
@@ -9,11 +11,12 @@ func Sort2Way(arr []int) {
 }
 
 func quickSort(arr []int, l, r int) {
-	if l < r {
-		pivot := partition(arr, l, r)
-		quickSort(arr, l, pivot-1)
-		quickSort(arr, pivot+1, r)
+	if l > r {
+		return
 	}
+	pivot := partition(arr, l, r)
+	quickSort(arr, l, pivot-1)
+	quickSort(arr, pivot+1, r)
 
 }
 
@@ -56,4 +59,64 @@ func partition(arr []int, l int, r int) int {
 	//		right--
 	//	}
 	//}
+}
+
+func Sort3way(arr []int) {
+	sortThreeWay(arr, 0, len(arr)-1)
+}
+
+func sortThreeWay(arr []int, l, r int) {
+	if l < r {
+		lt, gt := sort3Way(arr, l, r)
+		sortThreeWay(arr, l, lt-1)
+		sortThreeWay(arr, gt, r)
+	}
+}
+
+func sort3Way(arr []int, l, r int) (int, int) {
+	//if r -l < 15 {
+	//	insert.Sort(arr)
+	//}
+	/*pivot := arr[l]
+	lt := l - 1
+	gt := r + 1
+	i := 1
+	for i < gt {
+		if arr[i] == pivot {
+			i++
+		} else if arr[i] < pivot {
+			util.Swap(arr, i, lt+1)
+			i++
+			lt++
+		} else {
+			util.Swap(arr, i, gt-1)
+			gt--
+		}
+	}
+	sort3Way(arr, l, lt)
+	sort3Way(arr, gt, r)*/
+	v := arr[l]
+
+	lt := l     // arr[l+1...lt] < v
+	gt := r + 1 // arr[gt...r] > v
+	i := l + 1  // arr[lt+1...i) == v 半开半闭
+	for {
+		switch {
+		case i >= gt:
+			// 这步未执行前,lt都是指向最后一个小于v的值,
+			// 执行交换后,lt指向从左到右第一个等于v的值
+			arr[l], arr[lt] = arr[lt], v
+			return lt, gt
+		case arr[i] == v:
+			i++
+		case arr[i] < v:
+			lt++
+			arr[i], arr[lt] = arr[lt], arr[i]
+			i++
+		default:
+			gt--
+			arr[i], arr[gt] = arr[gt], arr[i]
+		}
+	}
+
 }
--- datastruxt-algoritem-go/algoritem/sort/quick/quick_test.go
+++ datastruxt-algoritem-go/algoritem/sort/quick/quick_test.go
@@ -19,19 +19,50 @@ func TestSort2Way(t *testing.T) {
 	}{
 		{
 			name: "quick sort 2 way",
-			args: args{util.GeneratorArray(10, 1, 10)},
+			args: args{util.GeneratorArray(100, 1, 100)},
 		},
 		{
 			name: "quick sort 2 way",
-			args: args{util.GeneratorArray(20, 1, 10)},
+			args: args{util.GeneratorArray(10000, 1, 1000)},
+		},
+		{
+			name: "quick sort 2 way",
+			args: args{util.GeneratorArray(1000000, 1, 10000)},
+		},
+		{
+			name: "quick sort 2 way",
+			args: args{util.GeneratorArray(10000000, 1, 10000)},
+		},
+		{
+			name: "quick sort 3 way",
+			args: args{util.GeneratorArray(100, 1, 100)},
+		},
+		{
+			name: "quick sort 3 way",
+			args: args{util.GeneratorArray(10000, 1, 1000)},
+		},
+		{
+			name: "quick sort 3 way",
+			args: args{util.GeneratorArray(1000000, 1, 10000)},
+		},
+		{
+			name: "quick sort 3 way",
+			args: args{util.GeneratorArray(10000000, 1, 10000)},
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			array := util.SortedArray(tt.args.arr)
-			Sort2Way(tt.args.arr)
-			fmt.Println(tt.name+" result: ", reflect.DeepEqual(array, tt.args.arr))
-			fmt.Println(tt.args.arr)
-		})
+	for i, tt := range tests {
+		if i <= 3 {
+			t.Run(tt.name, func(t *testing.T) {
+				array := util.SortedArray(tt.args.arr)
+				Sort2Way(tt.args.arr)
+				fmt.Println(tt.name+" result: ", reflect.DeepEqual(array, tt.args.arr))
+			})
+		} else {
+			t.Run(tt.name, func(t *testing.T) {
+				array := util.SortedArray(tt.args.arr)
+				Sort3way(tt.args.arr)
+				fmt.Println(tt.name+" result: ", reflect.DeepEqual(array, tt.args.arr))
+			})
+		}
 	}
 }
